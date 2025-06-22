ed0eaac144e2ef385086aea72486002691c016f6
Amneesh <amneeshsingh5@gmail.com>
2025-06-21 17:59:43 -0700

    final-v23

--- script.js
+++ script.js
@@ -546,3 +546,19 @@ window.onbeforeunload = function () {
 
 // // Optional: Hide controls and prevent autoplay on load
 // video.removeAttribute('autoplay');
+
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+  let paths = document.querySelectorAll("path");
+  let mode=repeat?'infinite':'forwards'
+  for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      const length = path.getTotalLength();
+      path.style["stroke-dashoffset"] = `${length}px`;
+      path.style["stroke-dasharray"] = `${length}px`;
+      path.style["stroke-width"] = `${strokeWidth}px`;
+      path.style["stroke"] = `${strokeColor}`;
+      path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+      path.style["animation-delay"] = `${i * delay}s`;
+  }
+}
+setTextAnimation(0.2,5.6,1,'ease-in-out','#f8db9c',false);
